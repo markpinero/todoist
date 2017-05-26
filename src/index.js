85ffd697d44cb936d1bf39d84e54cfa5e90a99d6
Mark Pinero <markspinero@gmail.com>
2017-05-25 22:31:33 -0700

    implemented store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from 'containers/App';
 import registerServiceWorker from 'registerServiceWorker';
+import configureStore from 'redux/configureStore';
 import 'index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
